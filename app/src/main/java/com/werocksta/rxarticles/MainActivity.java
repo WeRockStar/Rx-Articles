597e94a0e308d9948edf149bd6c95065c37b4da8
WeRockStar <vr_freestyle@outlook.com>
2016-08-11 20:15:40 +0700

    Add sample 01

--- app/src/main/java/com/werocksta/rxarticles/MainActivity.java
+++ app/src/main/java/com/werocksta/rxarticles/MainActivity.java
@@ -2,6 +2,11 @@ package com.werocksta.rxarticles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void sample01() {
+        Observable<Integer> sample = Observable.create(subscriber -> {
+            subscriber.onNext(10);
+        });
+
+        Subscription subscription = sample.subscribe(n -> Log.d("Number", "" + n));
     }
 }
