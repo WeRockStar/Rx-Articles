c637196fc94d978027b4394f41ab720f79e956e3
WeRockStar <vr_freestyle@outlook.com>
2016-08-13 17:27:30 +0700

    Refactor to lambda

--- app/src/main/java/com/werocksta/rxarticles/MainActivity.java
+++ app/src/main/java/com/werocksta/rxarticles/MainActivity.java
@@ -22,32 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void sample01() {
 
-        Observable<String> sampleString = Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("Reactive Programming");
-                subscriber.onError(new NullPointerException());
-                subscriber.onCompleted();
-            }
-        });
-
-        subscription = sampleString.subscribe(new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-                Log.d("Completed", "No more data");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.e("Error", e.getMessage());
-            }
-
-            @Override
-            public void onNext(String data) {
-                Log.d("Retrieve", data);
-            }
-        });
-
+        Observable.create(subscriber ->
+                subscriber.onNext("Reactive Programming")
+        ).subscribe(
+                data -> Log.d("Retrieve", (String) data),
+                throwable -> Log.e("Error", throwable.getMessage()),
+                () -> Log.d("Completed", "No more data")
+        );
     }
 
     @Override
